9f9effa3e8bace20eb1ff5e566d0d77cf29ea2f6
Timdows <tim@inexpro.nl>
2018-12-29 17:16:29 +0100

    Create video resizer function

---
+++ VideoResizer.ps1
@@ -0,0 +1,19 @@
+﻿Param (
+    [string]$inputDirectory = "",
+    [string]$outputDirectory = ""
+)
+
+Write-Host "--------------------------------"
+Write-Host "inputDirectory:" $inputDirectory
+Write-Host "outputDirectory:" $outputDirectory
+Write-Host "--------------------------------"
+
+Function Resize() {
+    Get-ChildItem -Path $inputDirectory -filter *.mp4 | foreach {
+        $command = "ffmpeg -i `"" + $_.FullName + "`" `"" + $outputDirectory + "\" + $_.BaseName + ".mp4`""
+        Write-Host $command
+        iex $command
+    }
+}
+
+Resize
