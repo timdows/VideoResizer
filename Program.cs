83f9caabfb683271a56012e617055d77e0ad45d0
Timdows <tim@inexpro.nl>
2019-07-01 13:33:47 +0200

    Change file modified date

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace VideoResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"C:\Users\Timdows\Desktop\2019 vidoes resized");
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file).Replace("VID_", string.Empty).Replace(".mp4", string.Empty);
+                var date = DateTime.ParseExact(fileName, "yyyyMMdd_HHmmss", null);
+
+                Console.WriteLine($"{file} {date}");
+
+                File.SetLastWriteTime(file, date);
+            }
+        }
+    }
+}
--- VideoResizer.ps1
+++ VideoResizer.ps1
@@ -9,8 +9,9 @@ Write-Host "outputDirectory:" $outputDirectory
 Write-Host "--------------------------------"
 
 Function Resize() {
-    Get-ChildItem -Path $inputDirectory -filter *.mp4 | foreach {
-        $command = "ffmpeg -i `"" + $_.FullName + "`" `"" + $outputDirectory + "\" + $_.BaseName + ".mp4`""
+    Get-ChildItem -Path $inputDirectory -filter *.mp4 | ForEach-Object {
+        #$command = "ffmpeg -i `"" + $_.FullName + "`" -vf `"scale='min(1600,iw)':-1`" `"" + $outputDirectory + "\" + $_.BaseName + ".mp4`""
+        $command = "ffmpeg -i `"" + $_.FullName + "`" -vcodec libx264 -crf 24 -vf `"scale='min(1024,iw)':-1`" `"" + $outputDirectory + "\" + $_.BaseName + ".mp4`""
         Write-Host $command
         Invoke-Expression $command
     }
